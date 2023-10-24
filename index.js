d2bc43fde57696b4de86e3acb4f92908eb82a200
Alessandro <aleworkingofficial@gmail.com>
2023-10-24 15:00:04 -0300

    Corrected the project

--- index.js
+++ index.js
@@ -3,14 +3,4 @@ function toggleMode(){
     html.classList.toggle('light')
 
     const img = document.querySelector('#profile img')
-
-    if(html.classList.contains('light')){
-        
-        img.setAttribute('src', './assets/avatar-light.png')
-        img.setAttribute('alt', 'Imagem de um homem sorrindo com óculos escuros')
-    }
-    else{
-        img.setAttribute('src', './assets/avatar.png')
-        img.setAttribute('alt', 'Imagem de um homem sorrindo com óculos')
-    }
 }
